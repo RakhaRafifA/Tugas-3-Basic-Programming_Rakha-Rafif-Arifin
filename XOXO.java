fcd98ce0e3fa5071a55336df3cf6699e5712540f
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-17 14:12:49 +0700

    Code awal xoxo

---
+++ XOXO.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Tugas4XO{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan huruf X dan O = ");
+        String kata = input.nextString();
+        System.out.println("Anda memasukan kata = " + kata);
+
+    }
+}
