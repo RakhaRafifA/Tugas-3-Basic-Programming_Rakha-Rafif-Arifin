408ee84af0c3bcd899f0536c088278bb010a307d
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-17 14:48:30 +0700

    test code awal

---
+++ Faktor Bilangan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class TugasMencariFaktor{
+    public static void main(String[] args){
+        int bilangan;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Bilangan yang akan di faktorisasi = ");
+        bilangan = input.nextInt();
+        System.out.println("Bilangan yang akan di faktorisasi = " + bilangan);
+    }
+}
