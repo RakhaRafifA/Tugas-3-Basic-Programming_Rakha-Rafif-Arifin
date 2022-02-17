2aa1c35eed6751c8a26d267482cd254852058074
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-17 14:29:50 +0700

    Code untuk x dan o

--- XOXO.java
+++ XOXO.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class Tugas4XO{
     public static void main(String[] args){
+        int x, o, lain;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Masukan huruf X dan O = ");
@@ -9,5 +10,26 @@ class Tugas4XO{
         System.out.println("Anda memasukan kata = " + kata);
 
         input.close();
+
+        kata = kata.toLowerCase();
+        for (int i = 0; i < kata.length(); i++){
+            char ch = kata.charAt(i);
+
+            if (ch == 'x' ){
+                x++;
+            }
+            else if (ch == 'o'){
+                o++;
+            }
+            else (ch != 'x' && ch != 'o'){
+                lain++;
+            }
+        }
+        if (x == o){
+            System.out.println("True");
+        }
+        else if (x != o){
+            System.out.println("False");
+        }
     }
 }
